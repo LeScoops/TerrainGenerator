430b8e54896193f74f9d556edc3ba552f566641d
Ben <34342294+LeScoops@users.noreply.github.com>
2020-11-16 12:12:40 -0500

    Initial Readme Upload
    Added a simple Readme file for other users to use tool

--- Free Exploration/Assets/Scripts/Terrain/Generation/GenerateDetails.cs
+++ Free Exploration/Assets/Scripts/Terrain/Generation/GenerateDetails.cs
@@ -17,30 +17,7 @@ public class GenerateDetails : MonoBehaviour
     //Smooth ---------------------------------------
     public int smoothIterations = 1;
 
-    //Vegetation ----------------------------------
-    //[System.Serializable]
-    //public class Vegetation
-    //{
-    //    public GameObject mesh;
-    //    public float minHeight = 0.1f;
-    //    public float maxHeight = 0.2f;
-    //    public float scattering = 5.0f;
-    //    public float minSlope = 0;
-    //    public float maxSlope = 90;
-    //    public float minScale = 0.5f;
-    //    public float maxScale = 1.0f;
-    //    public Color colour1 = Color.white;
-    //    public Color colour2 = Color.white;
-    //    public Color lightColour = Color.white;
-    //    public bool remove = false;
-    //}
-    //public List<Vegetation> vegetation = new List<Vegetation>()
-    //{
-    //    new Vegetation()
-    //};
-    //public int maximumTrees = 1000;
-    //public int treeSpacing = 5;
-
+    //Vegetation ----------------------------------    
     [SerializeField] SO_Trees treeValues = null;
 
     //Details --------------------------------------
